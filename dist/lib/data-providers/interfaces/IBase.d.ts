067cee7a444e2a12cf5346d58a9f2badd4984029
Lucas Souza <ls18017@gmail.com>
2025-06-17 08:44:58 -0300

    refactor: improve types

--- dist/lib/data-providers/interfaces/IBase.d.ts
+++ dist/lib/data-providers/interfaces/IBase.d.ts
@@ -2,11 +2,11 @@ export interface IBase<GType extends string> {
     /**
      * Uuid used as identifier
      */
-    readonly id: string;
+    id: string;
     /**
      * Used for tree it helps to know what type of item is this
      */
-    readonly type: GType;
+    type: GType;
     /**
      * Used to identify a record more simply.
      *
@@ -14,21 +14,9 @@ export interface IBase<GType extends string> {
      *  * Cannot have special characters
      *  * It cannot be empty
      */
-    readonly name: string;
+    name: string;
     /**
      * Used to describe some more specific detail
      */
-    readonly description: string | null;
-    /**
-    * Used to store all properties of an item
-    */
-    readonly properties: Map<string, unknown>;
-    /**
-     * Return a list of used names to do not use in new records
-     */
-    getProperty<GInitialValue>(fieldType: string, initialValue: GInitialValue): GInitialValue;
-    /**
-     * Used to transform in a object without observables.
-     */
-    toObject(): Record<string, unknown>;
+    description: string | null;
 }
